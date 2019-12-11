f82ec4aa2a12455a89d597c3a06fb622977eaac5
jenoshan <jenoshan4744@gmail.com>
2019-12-11 13:08:26 +0530

    menu bar

--- src/Home/HomeMenu.java
+++ src/Home/HomeMenu.java
@@ -3,23 +3,39 @@ package Home;
 import javax.swing.*;
 import java.awt.*;
 
-public class HomeMenu extends JFrame {
+public class HomeMenu extends JFrame{
 
-    JMenuBar mbar =new JMenuBar();
+    JMenuBar menubar = new JMenuBar();
 
-    @Override
-    public void setJMenuBar(JMenuBar jMenuBar) {
-        super.setJMenuBar(mbar);
+    public HomeMenu() {
+
+        setTitle("Administrator");
+        setJMenuBar(menubar);
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(screen.width,screen.height);
+        setLayout(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JMenu File = new JMenu("File");
+        menubar.add(File);
+
+        JMenuItem Home = new JMenuItem("Home");
+        JMenuItem Open = new JMenuItem("Open");
+        JMenuItem Exit = new JMenuItem("Exit");
+
+        File.add(Home);
+        File.add(Open);
+        File.add(Exit);
+
+        JMenu Color = new JMenu("Color");
+        menubar.add(Color);
+
+        JMenuItem Light = new JMenuItem("Light");
+        JMenuItem Dark = new JMenuItem("Dark");
+
+        Color.add(Light);
+        Color.add(Dark);
+
     }
-
-    public HomeMenu(){
-    super("Administrator");
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    setSize(screenSize.width,screenSize.height);
-    setLocation(140,140);
-    setResizable(true);
-    setVisible(true);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
-}
 }
